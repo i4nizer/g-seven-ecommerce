fb070d744bd96e17646bb7a61da9075039650675
i4nizer <iansandoval264@gmail.com>
2024-10-05 22:00:23 +0800

    Updated database

--- config/g_seven_ecommerce.sql
+++ config/g_seven_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 02, 2024 at 01:25 PM
+-- Generation Time: Oct 05, 2024 at 01:59 PM
 -- Server version: 8.0.30
 -- PHP Version: 8.1.10
 
@@ -75,6 +75,14 @@ CREATE TABLE `categories` (
   `description` text COLLATE utf8mb4_general_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `name`, `description`) VALUES
+(1, 'Shoes', 'Shoe collection'),
+(2, 'Green', 'Green collection');
+
 -- --------------------------------------------------------
 
 --
@@ -86,6 +94,19 @@ CREATE TABLE `category_products` (
   `category_id` int DEFAULT NULL,
   `product_id` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `category_products`
+--
+
+INSERT INTO `category_products` (`category_products_id`, `category_id`, `product_id`) VALUES
+(1, 2, 1),
+(2, 2, 2),
+(3, 2, 3),
+(4, 1, 4),
+(5, 1, 3),
+(6, 1, 2),
+(7, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -140,12 +161,47 @@ CREATE TABLE `products` (
   `product_id` int NOT NULL,
   `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `description` text COLLATE utf8mb4_general_ci,
-  `image_url` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `price` decimal(10,2) NOT NULL,
   `quantity` int NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`product_id`, `name`, `description`, `price`, `quantity`, `created_at`, `updated_at`) VALUES
+(1, 'Green Shoe', 'This is a shoe.', '200.00', 26, '2024-10-04 15:43:38', '2024-10-04 15:43:38'),
+(2, 'Green Shoe', 'This is a shoe.', '200.00', 26, '2024-10-04 15:43:45', '2024-10-04 15:43:45'),
+(3, 'Green Shoe', 'This is a shoe.', '200.00', 26, '2024-10-04 15:43:51', '2024-10-04 15:43:51'),
+(4, 'Green Shoe', 'This is a shoe.', '200.00', 26, '2024-10-04 15:43:55', '2024-10-04 15:43:55');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `product_image_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `attribute` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product_images`
+--
+
+INSERT INTO `product_images` (`product_image_id`, `product_id`, `url`, `attribute`, `created_at`, `updated_at`) VALUES
+(1, 1, '/img/shoe-green.png', 'green', '2024-10-05 14:35:00', '2024-10-05 14:35:00'),
+(2, 2, '/img/shoe-green.png', 'green', '2024-10-05 14:35:19', '2024-10-05 14:35:19'),
+(3, 3, '/img/shoe-green.png', 'green', '2024-10-05 14:35:25', '2024-10-05 14:35:47'),
+(4, 4, '/img/shoe-green.png', 'green', '2024-10-05 14:36:06', '2024-10-05 14:36:06'),
+(5, 1, '/img/shoe-green.png', 'green green shoe', '2024-10-05 15:50:03', '2024-10-05 15:56:53');
 
 -- --------------------------------------------------------
 
@@ -162,6 +218,13 @@ CREATE TABLE `reviews` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ;
 
+--
+-- Dumping data for table `reviews`
+--
+
+INSERT INTO `reviews` (`review_id`, `product_id`, `user_id`, `rating`, `comment`, `created_at`) VALUES
+(1, 1, 7, 4, 'The product was so green.', '2024-10-05 15:50:38');
+
 -- --------------------------------------------------------
 
 --
@@ -172,14 +235,21 @@ CREATE TABLE `users` (
   `user_id` int NOT NULL,
   `username` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `email` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `password` varchar(64) COLLATE utf8mb4_general_ci NOT NULL,
-  `first_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `last_name` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
-  `phone_number` varchar(20) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `password` varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `firstname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `lastname` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `phone` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `role` varchar(50) COLLATE utf8mb4_general_ci NOT NULL DEFAULT 'customer',
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP,
   `updated_at` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `firstname`, `lastname`, `phone`, `role`, `created_at`, `updated_at`) VALUES
+(7, 'jmack', 'jmack@gmail.com', 'ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f', 'James', 'Mack', '09105341892', 'customer', '2024-10-03 18:54:31', '2024-10-03 18:54:31');
 
 --
 -- Indexes for dumped tables
@@ -250,6 +320,13 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`product_image_id`),
+  ADD KEY `product_id` (`product_id`);
+
+--
 -- Indexes for table `reviews`
 --
 ALTER TABLE `reviews`
@@ -291,13 +368,13 @@ ALTER TABLE `cart_items`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `category_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `category_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `category_products`
 --
 ALTER TABLE `category_products`
-  MODIFY `category_products_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `category_products_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -321,7 +398,13 @@ ALTER TABLE `payments`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `product_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `product_image_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `reviews`
@@ -333,7 +416,7 @@ ALTER TABLE `reviews`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
@@ -383,6 +466,12 @@ ALTER TABLE `order_items`
 --
 ALTER TABLE `payments`
   ADD CONSTRAINT `payments_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`);
+
+--
+-- Constraints for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `reviews`
